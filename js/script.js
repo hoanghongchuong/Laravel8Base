f377b771682b839ee5598ab721560071e5efe80a
hoanghoangchuong <chuonghoanghong@gmail.com>
2021-05-28 22:29:57 +0700

    html

--- js/script.js
+++ js/script.js
@@ -1,27 +1,22 @@
-$(document).ready(function($){	
-	var owl = $(".owl-carousel-product");
-	  	owl.owlCarousel({
-	  		margin:0, 							
-	  		loop:true, 							
-	  		nav:true, 							
-	  		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'], 
-	  		autoplay:false, 						
-	  		autoplayTimeout:1500,
-			autoplayHoverPause:true,
-			autoplaySpeed: 2000,
-			responsiveClass:true, 				
-		    responsive:{
-		        0:{
-		            items:2,									            									            
-		        },
-		        600:{
-		            items:2,          
-		        },
-		        1000:{
-		            items:4,  
-		        }
-		    }
-		});
+$(document).ready(function($){
+	$('.owl-carousel').owlCarousel({
+		loop:false,
+		margin:10,
+		nav: false,
+		dots: true,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:3
+			}
+		}
+	})
 
 	  	$(".owl-carousel-feedback").owlCarousel({
 	  		margin:0, 							
